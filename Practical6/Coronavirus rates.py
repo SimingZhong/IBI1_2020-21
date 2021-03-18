5421cf133396c38f336874831e6a871623500e86
SimingZhong <siming.20@intl.zju.edu.cn>
2021-03-18 18:05:08 +0800

    Coronavirus rates.py

--- Practical6/Coronavirus rates.py
+++ Practical6/Coronavirus rates.py
@@ -6,10 +6,10 @@ countries = {'USA': 29862124, 'India': 11285561, 'Brazil': 11205972, 'Russia': 4
 
 #Draw a pie chart.
 
-import matplotlib.pyplot as plt
-labels = 'USA', 'India', 'Brazil', 'Russia', 'UK'
-sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
-explode = (0.1, 0.1, 0.1, 0.1, 0.1)
+import matplotlib.pyplot as plt #Import matplotlib
+labels = 'USA', 'India', 'Brazil', 'Russia', 'UK' #What the labels are
+sizes = [29862124, 11285561, 11205972, 4360823, 4234924] #What are the corresponding numbers
+explode = (0.1, 0.1, 0.1, 0.1, 0.1) #Decide which part should split apart.
 plt.pie(sizes,explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-plt.axis('equal')
-plt.show()
+plt.axis('equal') #Design the axes.
+plt.show() #Show the pie chart.
