c5a111440a42ff049db29661303c511f8d5e7d85
SimingZhong <79953089+SimingZhong@users.noreply.github.com>
2021-05-19 12:23:27 +0800

    Update Student list.py

--- Practical10/Student list.py
+++ Practical10/Student list.py
@@ -1,14 +1,14 @@
 class Student(object):
-    def __init__(self,first_name, last_name, programme):
+    def __init__(self,first_name, last_name, programme): #define this class
         self.first_name = first_name
         self.last_name = last_name
         self.programme = programme
-    def information(self):
+    def information(self): #define a function in this class to print students' information
         info = self.first_name + '.' + self.last_name + ' ' + self.programme
         return info
 
 
 
-me = Student('Siming', 'Zhong', 'BMS')
+me = Student('Siming', 'Zhong', 'BMS') #input a student
 
 print(me.information())
