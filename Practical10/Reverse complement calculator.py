b4a85670f04fbed4757973dd09fac8b353b84d5e
SimingZhong <79953089+SimingZhong@users.noreply.github.com>
2021-05-19 12:23:04 +0800

    Update Reverse complement calculator.py

--- Practical10/Reverse complement calculator.py
+++ Practical10/Reverse complement calculator.py
@@ -1,15 +1,15 @@
-DNA_seq = 'atGCGACTACGATCGAgggCC'
+DNA_seq = 'atGCGACTACGATCGAgggCC' #input DNA sequence
 
 
-def reverse_complement_calculator(origin_seq):
-    inter_seq1 = origin_seq.upper()
+def reverse_complement_calculator(origin_seq): #define reverse complement calculator function
+    inter_seq1 = origin_seq.upper() #change all the letters into upper letters
     inter_seq2 = []
-    complement_table = {'A': 'T', 'G': 'C', 'C': 'G', 'T': 'A'}
+    complement_table = {'A': 'T', 'G': 'C', 'C': 'G', 'T': 'A'} #input dictionary(complement table)
     for i in range(len(inter_seq1)):
         inter_seq2 += list(complement_table[inter_seq1[i]])
-    inter_seq2.reverse()
-    final_seq = ''.join(inter_seq2)
+    inter_seq2.reverse() #reverse the list
+    final_seq = ''.join(inter_seq2) #combine the lists
     print(final_seq)
     return
 
-reverse_complement_calculator(DNA_seq)
+reverse_complement_calculator(DNA_seq) #run this function
