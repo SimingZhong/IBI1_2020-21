214919a8e93e7e21f0a621d0741b891a0a691e41
SimingZhong <79953089+SimingZhong@users.noreply.github.com>
2021-05-19 12:19:42 +0800

    Update get_unknown_function.py

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -1,19 +1,19 @@
 import os
 import re
 
-os.chdir("C:\\Users\嘤嘤怪的一朵玫瑰花\IBI1_2020-21\Practical8")
+os.chdir("C:\\Users\嘤嘤怪的一朵玫瑰花\IBI1_2020-21\Practical8") #change work directory
 
-sequence = ''
-with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') as fin, open('unknown_function.fa', 'w') as fout:
+sequence = '' #define sequence
+with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') as fin, open('unknown_function.fa', 'w') as fout: #read the original file and write a new file
     for line in fin:
-        if line.startswith('>') and re.search('unknown function', line):
-            new_name = re.findall(r'gene:(\S+)', line)
+        if line.startswith('>') and re.search('unknown function', line): #find the line with unknown function
+            new_name = re.findall(r'gene:(\S+)', line) #extract the gene name
             while True:
-                line = next(fin)
-                if not line.startswith('>'):
-                    sequence += line.replace('\n', '')
+                line = next(fin) #go to the next line
+                if not line.startswith('>'): #if this line is sequence line
+                    sequence += line.replace('\n', '') #combine the sequence
                 else:
                     break
-            length = len(sequence)
-            fout.write('\n' + '>>' + str(new_name) + '             ' + str(length))
+            length = len(sequence) #calculate the length
+            fout.write('\n' + '>>' + ''.join(new_name) + '             ' + str(length))
             fout.write('\n' + str(sequence))
