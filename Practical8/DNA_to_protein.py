55d167d441cb6ff69d165f200849912fe25b9420
SimingZhong <79953089+SimingZhong@users.noreply.github.com>
2021-05-19 12:18:42 +0800

    Update DNA_to_protein.py

--- Practical8/DNA_to_protein.py
+++ Practical8/DNA_to_protein.py
@@ -1,5 +1,6 @@
 import re
-seq = 'ATGCGACTACGATCGAGGGCC'
+seq = 'ATGCGACTACGATCGAGGGCC' #input DNA sequence
+#write genetic code table
 genetic_code_table = {'TTT':'F','TTC':'F','TTA':'L','TTG':'L',
          'CTT':'L','CTC':'L','CTA':'L','CTG':'L',
          'ATT':'I','ATC':'I','ATA':'J','ATG':'M',
@@ -16,8 +17,8 @@ genetic_code_table = {'TTT':'F','TTC':'F','TTA':'L','TTG':'L',
          'CGT':'R','CGC':'R','CGA':'R','CGG':'R',
          'AGT':'S','AGC':'S','AGA':'R','AGG':'R',
          'GGT':'G','GGC':'G','GGA':'G','GGG':'G'}
-DNA_codons = re.findall('...',seq)
+DNA_codons = re.findall('...',seq)#extract the DNA codons from the sequence
 print('The protein sequence is ', end='')
-for i in range(len(DNA_codons)):
+for i in range(len(DNA_codons)): #translate the DNA codons into proteins
     print(genetic_code_table[DNA_codons[i]], end = '')
 print('.')
