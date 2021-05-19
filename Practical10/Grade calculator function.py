9ee0e21c90e2ccc2153d0a400bc191ab3e213c57
SimingZhong <79953089+SimingZhong@users.noreply.github.com>
2021-05-19 12:22:40 +0800

    Update Grade calculator function.py

--- Practical10/Grade calculator function.py
+++ Practical10/Grade calculator function.py
@@ -1,7 +1,7 @@
-def grade_calculator(name, code_grade, poster_grade, exam_grade):
-    final_grade = 0.4*code_grade + 0.3*poster_grade + 0.3*exam_grade
+def grade_calculator(name, code_grade, poster_grade, exam_grade): #define a function
+    final_grade = 0.4*code_grade + 0.3*poster_grade + 0.3*exam_grade #calculate the grade
     print(name, final_grade)
     return
 
 #name is in string type
-grade_calculator('Siming', 100,90,100)
+grade_calculator('Ben', 100,90,100)
